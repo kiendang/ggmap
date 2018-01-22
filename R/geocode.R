bd9a236dc6ae825a9aeb502da95cecddf42a84e3
Dang Trung Kien <kiendang@users.noreply.github.com>
2018-01-22 20:53:33 +0800

    Fix for standard users

--- R/geocode.R
+++ R/geocode.R
@@ -138,11 +138,7 @@ geocode <- function(location, output = c("latlon", "latlona", "more", "all"),
   if(source == "google"){
 
     # add google account stuff
-    if (has_goog_client() && has_goog_signature()) {
-      client <- goog_client()
-      signature <- goog_signature()
-      posturl <- paste(posturl, fmteq(client), fmteq(signature), sep = "&")
-    } else if (has_goog_key()) {
+    if (has_goog_key()) {
       key <- goog_key()
       posturl <- paste(posturl, fmteq(key), sep = "&")
     }
